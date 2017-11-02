8681bf79e5c230938a78fed603cb36cfd21d7f50
admin <admin@TA-822-08>
2017-11-02 14:15:45 +0100

    mba

---
+++ 10 tabel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int xx = 1; xx < 11; xx++)
+            {
+                for (int yy = 1; yy < 11; yy++)
+                {
+                    string tal = (xx * yy).ToString();
+                    if ((xx * yy) > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        if ((xx * yy) > 25)
+                            Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        if ((xx * yy) > 7)
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write(tal.PadLeft(4));
+
+                }
+                Console.WriteLine();
+
+                Console.ReadKey();  
+
+            }
+
+        }
+    }
+}
