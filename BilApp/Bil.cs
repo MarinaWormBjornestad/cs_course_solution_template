4950a2d5da277a6fe51d6391b5b5e27eeafd5bc4
Marina Bjornestad <marina@worm-bjornestad.dk>
2017-11-27 09:59:56 +0100

    mba

---
+++ BilApp/Bil.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BilApp
+{
+    public class Bil
+    {
+
+        //public int BilId;
+        public int BilId { get; set; }
+        public string Model { get; set; }
+
+        //public int AntalCylindre { get; set; }
+
+        private int antalCylindre;
+
+        public int AntalCylindre    
+        {
+            get { return AntalCylindre; }
+            set {
+                if (value < 4 || value > 8)
+                    throw new ApplicationException("Forkert antal cylindre");   
+
+               
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Bil :" + this.Model);
+        }
+
+    }
+
+    public class PersonBil : Bil {
+        public int AntalBarnesæder { get; set; }
+    }
+
+    public class LastBil : Bil {
+        public int LasteEvne { get; set; }
+    }
+}
+
+ 
---
+++ BilApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bil b = new Bil();
+            b.BilId = 1;
+            b.Model = "X";
+            b.AntalCylindre = 4;
+
+            Console.WriteLine("Ny bil");
+            Console.ReadKey();
+
+            LastBil a = new LastBil();
+            a.BilId = 2;
+            a.Model = "Y";
+            a.AntalCylindre = 8;
+
+            b.Print();
+            a.Print();
+
+            Console.ReadKey();
+
+        }
+    }
+}
