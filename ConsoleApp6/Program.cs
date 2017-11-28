e419c5387b49ddff2da2be40704c0e3bfa672326
Marina Bjornestad <marina@worm-bjornestad.dk>
2017-11-28 09:05:01 +0100

    mba

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp6
         static void Main(string[] args)
         {
             Bunke b = new Bunke();
-            b.TilføjKort(new Kort() { Kulør = "Spar", Værdi = 2 });
+            b.TilføjKort(new Kort() { Kulør = "Spar", Værdi =        2 });
             b.TilføjKort(new Kort() { Kulør = "Hjerter", Værdi = 14 });
             b.TilføjKort(new Kort() { Kulør = "Ruder", Værdi = 3 });
             b.Vis();
